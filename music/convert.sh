28f34f843e8cee605f10f0f94de73342dab86d1e
david hamp-gonsalves <dhamp-gonsalves@gitlab.com>
2025-08-02 09:42:51 -0300

    sounds

--- music/convert.sh
+++ music/convert.sh
@@ -2,4 +2,6 @@ ffmpeg -i title.wav -ac 1 -ar 8000 -acodec pcm_s16le ../sd-card/music/title.wav
 ffmpeg -i choir.wav -ac 1 -ar 8000 -acodec pcm_s16le ../sd-card/music/choir.wav
 ffmpeg -i sparky.wav -ac 1 -ar 8000 -acodec pcm_s16le ../sd-card/music/sparky.wav
 ffmpeg -i spooky.wav -ac 1 -ar 8000 -acodec pcm_s16le ../sd-card/music/spooky.wav
+
 ffmpeg -i game-over.wav -ac 1 -ar 8000 -acodec pcm_s16le ../sd-card/sounds/game-over.wav
+ffmpeg -i get-ready.wav -ac 1 -ar 8000 -acodec pcm_s16le ../sd-card/sounds/get-ready.wav
